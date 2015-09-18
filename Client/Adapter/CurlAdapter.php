6b38e8476622886200c8f9a606cb8afe32f2d0a7
Sergei Vizel <sergei.vizel@modera.net>
2015-09-18 14:10:08 +0300

    CurlAdapter rewrited

--- Client/Adapter/CurlAdapter.php
+++ Client/Adapter/CurlAdapter.php
@@ -2,11 +2,19 @@
 
 namespace Cravler\FayeAppBundle\Client\Adapter;
 
-use Nc\FayeClient\Adapter\CurlAdapter as BaseAdapter;
-
 /**
  * @author Sergei Vizel <sergei.vizel@gmail.com>
  */
-class CurlAdapter extends BaseAdapter
+class CurlAdapter implements AdapterInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function postJSON($url, $body)
+    {
+        $cmd = "curl -X POST -H 'Content-Type: application/json'";
+        $cmd.= " -d '" . $body . "' " . "'" . $url . "'";
+        $cmd .= " > /dev/null 2>&1 &";
+        exec($cmd, $output, $exit);
+    }
 }
