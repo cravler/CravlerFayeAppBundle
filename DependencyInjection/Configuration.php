84455906045dfe1f0a210220f3a7de7e15d38373
Sergei Vizel <sergei.vizel@modera.net>
2019-11-20 11:07:48 +0200

    sf4 compatibility

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cravler_faye_app');
+        $treeBuilder = new TreeBuilder('cravler_faye_app');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('cravler_faye_app');
+        }
 
         $rootNode
             ->children()
